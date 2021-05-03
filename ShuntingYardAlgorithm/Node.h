a2af2a787a82bba74acce6e5ad1d2d47060972a7
brianfang203 <79074843+brianfang203@users.noreply.github.com>
2021-05-02 23:50:37 -0700

    Create Node.h

---
+++ ShuntingYardAlgorithm/Node.h
@@ -0,0 +1,24 @@
+#ifndef NODE_H
+#define NODE_H
+
+using namespace std;
+
+class Node {
+ public:
+  Node();
+  ~Node();
+  void setRight(Node*);
+  void setLeft(Node*);
+  void setNext(Node*);
+  Node* getRight();
+  Node* getLeft();
+  Node* getNext();
+  void setData(char*);
+  char* getData();
+ private:
+  Node* left;
+  Node* right;
+  Node* next;
+  char* data;
+};
+#endif
