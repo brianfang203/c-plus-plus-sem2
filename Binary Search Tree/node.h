aae4a0c03da8d88dd0bd70f1b4a603a2202880b7
brianfang203 <79074843+brianfang203@users.noreply.github.com>
2021-05-17 11:00:56 -0700

    Update node.h

--- Binary Search Tree/node.h
+++ Binary Search Tree/node.h
@@ -1,1 +1,27 @@
+#ifndef Node_h
+#define Node_h
 
+#include <iostream>
+
+using namespace std;
+
+class Node {
+ public:
+  Node();
+  ~Node();
+  Node* getParent();
+  Node* getLeft();
+  Node* getRight();
+  int getValue();
+  void setParent(Node*);
+  void setLeft(Node*);
+  void setRight(Node*);
+  void setValue(int);
+ private:
+  Node* parent;
+  Node* left;
+  Node* right;
+  int value;
+};
+
+#endif
