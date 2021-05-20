7b749bf816e10c2c8f3b47acfcde71217904e831
brianfang203 <79074843+brianfang203@users.noreply.github.com>
2021-05-19 17:33:35 -0700

    Update main.cpp

--- Binary Search Tree/main.cpp
+++ Binary Search Tree/main.cpp
@@ -46,21 +46,21 @@ int main() {
 		siz = 0;
 		count = 0;  
 
-    //getting user input
+    		//getting user input
 		cout << endl << "What would you like to do(tree/quit): " << endl;
 		cin.get(inp, 10);
 		cin.clear();
 		cin.ignore(10000, '\n');
 		if (strcmp(inp, "tree") == 0) {
 
-      //console or file input
+      			//console or file input
 			cout << "Enter \"type\" for console input or \"file\" for file input: " << endl;
 			cin.get(inp, 10);
 			cin.clear();
 			cin.ignore(10000, '\n');
 			bool tr = false;
 
-      //if file input
+      			//if file input
 			if (strcmp(inp, "file") == 0) {
 				tr = true;
 				cout << endl << "Enter the name of the file: " << endl << ">> ";
@@ -84,7 +84,7 @@ int main() {
 				}
 			}
 
-      //if console input
+      			//if console input
 			else if (strcmp(inp, "type") == 0) {
 				tr = true;
 				cout << "Enter numbers with spaces inbetween: " << endl;
@@ -94,7 +94,7 @@ int main() {
 				cin.ignore(1000000, '\n');
 				parse(in, modif, count);  
 
-        //find size
+        			//find size
 				for (int i = 0; i < 100; i++) {
 					if (modif[i] != 0) {
 						siz++;
@@ -103,13 +103,13 @@ int main() {
 				cout << endl;      
 			}
 
-      //invalid input
+      			//invalid input
 			else {
 				tr = false;
 				cout << "Invalid input" << endl;
 			}
 
-      //creating tree
+      			//creating tree
 			if (tr) {
 				Node* head = NULL;  
 				Node* curr = NULL;  
@@ -121,20 +121,20 @@ int main() {
 					}
 				}
 
-        //print tree
+        			//print tree
 				printTree(head, NULL, false);
 				cout << endl;
 				bool r = true;
 				char resp[10];
 
-        //while program is still running
+        			//while program is still running
 				while (r) {
 					cout << endl << "Would you like to search, add, remove, or done: " << endl;
 					cin.get(resp, 10);
 					cin.clear();
 					cin.ignore(10000, '\n');
 
-          //search in tree
+          				//search in tree
 					if (strcmp(resp, "search") == 0) {
 						int val;
 						cout << "Search value: ";
@@ -143,18 +143,18 @@ int main() {
 						cin.ignore(10000, '\n');
 						Node* temp = search(head, val);
 
-            //in tree
+            					//in tree
 						if (temp != NULL) {
 							cout << endl << val << " is part of the tree" << endl;
 						} 
-            //not in tree
-            else {
+            					//not in tree
+            					else {
 							cout << endl << val << " is not par of the tree" << endl;
 						}
 					} 
 
-          //remove from tree
-          else if (strcmp(resp, "remove") == 0) {
+          				//remove from tree
+          				else if (strcmp(resp, "remove") == 0) {
 						int val;
 						cout << "Remove value: ";
 						cin >> val;
@@ -166,8 +166,8 @@ int main() {
 						cout << endl;
 					} 
 
-          //add to tree
-          else if (strcmp(resp, "add") == 0) {
+          				//add to tree
+          				else if (strcmp(resp, "add") == 0) {
 						int val;
 						cout << "Add value: ";
 						cin >> val;
@@ -182,26 +182,26 @@ int main() {
 						cout << endl;
 					} 
 
-          //finish tree
-          else if (strcmp(resp, "done") == 0) {
+          				//finish tree
+          				else if (strcmp(resp, "done") == 0) {
 						r = false;
 					} 
 
-          //invalid input
-          else {
+          				//invalid input
+          				else {
 						cout << endl << "Invalid input" << endl;
 					}
 				}
 			}
 		}
 
-    //quitting program
+    		//quitting program
 		else if (strcmp(inp, "quit") == 0) {
 			cout << endl << "Quitting program" << endl;
 			run = false;
 		}
 
-    //invalid input
+    		//invalid input
 		else {
 			cout << endl << "Invalid input" << endl;
 		}
@@ -221,7 +221,7 @@ void parse(char* in, int* modif, int &count) {
 				count++;
 				l = 0;
 			} 
-      else {
+      			else {
 				int temp = 0;
 				for (int a = 0; a < l; a++) {
 					temp = 10 * temp + (in[i-l+a] - '0');
@@ -231,7 +231,7 @@ void parse(char* in, int* modif, int &count) {
 				l = 0;
 			}
 		} 
-    else {
+    		else {
 			int temp = 0;
 			l++;
 			if (i == strlen(in) - 1) { 
@@ -252,7 +252,7 @@ void insert(Node* &head, Node* &curr, Node*& prev, int val) {
 		head->setData(val);
 		return;
 	} 
-  else {
+  	else {
 		if (val < curr->getData()) {  
 			prev = curr;
 			curr = curr->getLeft();
@@ -262,11 +262,11 @@ void insert(Node* &head, Node* &curr, Node*& prev, int val) {
 				prev->setLeft(curr);
 				return;
 			} 
-      else {  
+      			else {  
 				insert(head, curr, prev, val);
 			}
 		} 
-    else {
+    		else {
 			prev = curr;
 			curr = curr->getRight();  
 			if (curr == NULL) {  
@@ -275,7 +275,7 @@ void insert(Node* &head, Node* &curr, Node*& prev, int val) {
 				prev->setRight(curr);
 				return;
 			} 
-      else { 
+      			else { 
 				insert(head, curr, prev, val);
 			}
 		}
@@ -320,15 +320,15 @@ Node* search(Node* curr, int val) {
 		if (curr->getLeft() != NULL) {
 			search(curr->getLeft(), val); 
 		} 
-    else {
+    		else {
 			return NULL; 
 		}
 	} 
-  else {
+  	else {
 		if (curr->getRight() != NULL) {
 			search(curr->getRight(), val); 
 		} 
-    else {
+    		else {
 			return NULL;  
 		}
 	} 
