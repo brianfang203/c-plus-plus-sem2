edfb086e8a460f00ffb7c0284df9dd583e7962e2
brianfang203 <79074843+brianfang203@users.noreply.github.com>
2021-06-14 09:20:55 -0700

    Create vertex.cpp

---
+++ GraphCreator/vertex.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "vertex.h"
+
+using namespace std;
+
+Vertex::Vertex(char inLabel) {
+  label = inLabel;
+  distance = -1;
+  previous = NULL;
+}
+
+Vertex::~Vertex() {
+  delete &label;
+  delete &distance;
+  delete previous;
+}
+
+char Vertex::getLabel() {
+  return label;
+}
+
+int Vertex::getDistance() {
+  return distance;
+}
+
+Vertex* Vertex::getPrevious() {
+  return previous;
+}
+
+void Vertex::setLabel(char inLabel) {
+  label = inLabel;
+}
+
+void Vertex::setDistance(int inDistance) {
+  distance = inDistance;
+}
+
+void Vertex::setPrevious(Vertex* inPrevious) {
+  previous = inPrevious;
+}
